727365c5e08ffdc6464817de67941a1eb8128b5a
Pablo levy <pablo.levy@epitech.eu>
2021-10-20 10:31:05 +0200

    add sound with segfault

--- main.c
+++ main.c
@@ -21,10 +21,13 @@ int main(int ac, char * const *av)
 {
     sfRenderWindow *window = create_window(800 , 600, 32 , "Csfml");
     sfEvent event;
-
+    sfMusic_createFromFile("113 Nightmare.ogg");
+    sfMu
     while (sfRenderWindow_isOpen(window)) {
         sfRenderWindow_clear(window, sfRed);
         sfRenderWindow_display(window);
+        sfMusic_setLoop("113 Nightmare.ogg", sfTrue);
+        sfMusic_play("113 Nightmare.ogg");
         while (sfRenderWindow_pollEvent(window, &event)) {
             if (event.type == sfEvtClosed)
                 sfRenderWindow_close(window);
